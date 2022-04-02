45196ae1fa22be5759b55e92edb275e1fc17c94d
Ing. Patricia Avalos <100172494+PatoAvalos@users.noreply.github.com>
2022-04-02 01:40:15 -0600

    Add files via upload

---
+++ variablesFormulario.js
@@ -0,0 +1,9 @@
+const inputNombre = document.querySelector("#inputNombre");
+const inputEmail = document.querySelector("#inputEmail");
+const inputSubject = document.querySelector("#inputSubject");
+const inputMessage = document.querySelector("#inputMessage");
+
+let datosdeInput = ""
+
+
+
---
+++ variablesjson.js
@@ -0,0 +1,5 @@
+const newservices = document.querySelector("#newservices")
+const descripcionServicio = document.querySelector("#descripcionServicio")
+const precioServicio = document.querySelector("#precioServicio")
+const btnGuardarServicio = document.querySelector("#btnGuardarServicio")
+
